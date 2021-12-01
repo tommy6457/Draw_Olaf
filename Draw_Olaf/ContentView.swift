4f10a86051def71d24a14e9f94be6b35ea1e7603
tommy <tommy5566123@gmail.com>
2021-12-01 09:39:53 +0800

    Initial Commit

---
+++ Draw_Olaf/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Draw_Olaf
+//
+//  Created by 蔡尚諺 on 2021/12/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Draw_Olaf/Draw_OlafApp.swift
@@ -0,0 +1,17 @@
+//
+//  Draw_OlafApp.swift
+//  Draw_Olaf
+//
+//  Created by 蔡尚諺 on 2021/12/1.
+//
+
+import SwiftUI
+
+@main
+struct Draw_OlafApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
